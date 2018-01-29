b55cf8adb89811b1b91283bdd161f6e0aade3890
Paulien van Alst <paulien@openvalue.nl>
2018-01-29 22:17:50 +0100

    KOTLIN - added class with extension functions

--- src/main/kotlin/com/paulienvanalst/rugbymatch/game/Game.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/game/Game.kt
@@ -1,2 +1,22 @@
-package com.paulienvanalst.rugbymatch
+package com.paulienvanalst.rugbymatch.game
+
+
+// deze class heeft state
+class Game (private var score: Score, private var time: Int) {
+
+
+}
+
+// extension functions
+class Score (private var home: Pair<String, Int>, private var visitors: Pair<String, Int>){
+    override fun toString(): String {
+//        return home.first + " " +  home.second + " - " + visitors.second + " " + visitors.first
+
+        return "${home.team()} ${home.points()} - ${visitors.points()} ${visitors.team()}"
+    }
+
+    private fun Pair<String, Int>.team() = this.first
+    private fun Pair<String, Int>.points() = this.second
+}
+
 
