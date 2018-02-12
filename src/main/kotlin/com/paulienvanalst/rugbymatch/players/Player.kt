27f499f8b5aecc71bad7d49856780d2641659e9e
Paulien van Alst <paulien@openvalue.nl>
2018-02-12 10:03:11 +0100

    [KOTLIN] - some extra work on Player class

--- src/main/kotlin/com/paulienvanalst/rugbymatch/players/Player.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/players/Player.kt
@@ -1,11 +1,29 @@
 package com.paulienvanalst.rugbymatch.players
 
+import org.apache.logging.log4j.LogManager
+
+private val logger = LogManager.getLogger(Player::class.java)
+
 enum class Team { HARLEQUINS, WASPS, SARACENS }
 
 interface Player {
     val team : Team
     val position : Position
     val isValid : Boolean
+
+//    fun run()
+//
+//    fun scrum()
+//
+//    fun playLineOut()
+//
+//    fun pass()
+
+    fun tackle(opponent: Player) : Player {
+        if (this.team != opponent.team) {
+            logger.error("Please do not tackle a player of your own team ... ")
+        }
+    }
 }
 
 data class Forward (override val team: Team, override val position: Position) : Player {
