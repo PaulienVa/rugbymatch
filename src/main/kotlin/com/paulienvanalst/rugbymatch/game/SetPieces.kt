829beb1cb05cb5d5f0c6a61ba340c313c5fba603
Paulien van Alst <paulien@openvalue.nl>
2018-03-14 23:31:59 +0100

    [KOTLIN] - reverted all the code not relevant for ex 2

--- src/main/kotlin/com/paulienvanalst/rugbymatch/game/SetPieces.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/game/SetPieces.kt
@@ -1,23 +1,7 @@
 package com.paulienvanalst.rugbymatch.game
 
-import com.paulienvanalst.rugbymatch.team.Team
 
-// Ex 2
 interface SetPiece {
-    val teamThrowingIn: Team
-    val otherTeam: Team
-
-    fun isValid() : Boolean
+   //todo to implement
 }
 
-data class Scrum(override val teamThrowingIn : Team, override val otherTeam: Team) : SetPiece {
-    override fun isValid() : Boolean{
-       return teamThrowingIn.hasScrum() and otherTeam.hasScrum()
-    }
-
-}
-data class LineOut(override val teamThrowingIn : Team, override val otherTeam: Team, val numberOfPlayersTeamThrowingIn: Int, val numberOfPlayersOtherTeam: Int) : SetPiece {
-    override fun isValid(): Boolean {
-        return numberOfPlayersOtherTeam == numberOfPlayersOtherTeam
-    }
-}
