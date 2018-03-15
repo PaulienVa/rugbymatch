cf2cff4698a3badef2bbbee6b417ec8a15dfc501
Paulien van Alst <paulien@openvalue.nl>
2018-03-15 19:23:17 +0100

    [KOTLIN] - Fixed the solution branch by removing all the database

--- src/main/kotlin/com/paulienvanalst/rugbymatch/analytics/MailingService.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/analytics/MailingService.kt
@@ -1,15 +1,7 @@
 package com.paulienvanalst.rugbymatch.analytics
 
-import com.paulienvanalst.rugbymatch.events.lineOutEvents
-import com.paulienvanalst.rugbymatch.events.lostBy
-import com.paulienvanalst.rugbymatch.events.scrumEvents
-import com.paulienvanalst.rugbymatch.events.wonBy
-import com.paulienvanalst.rugbymatch.team.TeamName
-import org.apache.ibatis.annotations.Param
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Repository
 
 @Component
 class MailingService {
