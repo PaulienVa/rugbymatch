473605f956ac275c7b211dbbc66f1a1a169f5731
Paulien van Alst <paulien@openvalue.nl>
2018-04-23 22:45:29 +0200

    [KOTLIN] - Fixed the IT class to make it work for parallel tests in ScoringBoardIT
    (cherry picked from commit 676216a)

--- src/main/kotlin/com/paulienvanalst/rugbymatch/game/ScoringBoard.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/game/ScoringBoard.kt
@@ -1,13 +1,24 @@
 package com.paulienvanalst.rugbymatch.game
 
+import com.paulienvanalst.rugbymatch.events.ScoringEvent
+import com.paulienvanalst.rugbymatch.events.StartGame
+import com.paulienvanalst.rugbymatch.team.TeamName
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class ScoringBoard {
+
+    private var scoringHistory : List<Score> = emptyList()
+
     fun currentScore(): GameScore {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    fun clear () {
+        scoringHistory = emptyList()
+
+    }
 }
 
 
