bd7fee4e22d3415b6bfb941432016428724ad43c
Paulien <>
2018-06-18 21:32:24 +0200

    [KOTLIN] - removed unnecessary code for this exercise

--- analytics-app/src/app/game/Teams.ts
+++
@@ -1,9 +0,0 @@
-export class Teams {
-  public hostingTeam: string;
-  public visitingTeam: string;
-
-  constructor(hostingTeam: string, visitingTeam: string){
-    this.hostingTeam = hostingTeam;
-    this.visitingTeam = visitingTeam;
-  }
-}
