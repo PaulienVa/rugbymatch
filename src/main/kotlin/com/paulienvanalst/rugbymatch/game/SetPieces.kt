6eb621281969e529b7b85bad64f25778db7b72df
Paulien <>
2018-09-17 23:12:34 +0200

    [KOTLIN] - Ex3 removed unnecessary comments

--- src/main/kotlin/com/paulienvanalst/rugbymatch/game/SetPieces.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/game/SetPieces.kt
@@ -2,25 +2,6 @@ package com.paulienvanalst.rugbymatch.game
 
 import com.paulienvanalst.rugbymatch.team.Team
 
-//interface SetPiece {
-//    val teamThrowingIn: Team
-//    val otherTeam: Team
-//
-//    fun isValid() : Boolean
-//}
-//
-//data class Scrum(override val teamThrowingIn : Team, override val otherTeam: Team) : SetPiece {
-//    override fun isValid() : Boolean{
-//       return teamThrowingIn.hasEnoughScrumPlayersInPlay() and otherTeam.hasEnoughScrumPlayersInPlay()
-//    }
-//
-//}
-//data class LineOut(override val teamThrowingIn : Team, override val otherTeam: Team, val numberOfPlayersTeamThrowingIn: Int, val numberOfPlayersOtherTeam: Int) : SetPiece {
-//    override fun isValid(): Boolean {
-//        return numberOfPlayersTeamThrowingIn == numberOfPlayersOtherTeam
-//    }
-//}
-
 interface SetPiece {
     val attackingTeam: Team
     val defendingTeam: Team
