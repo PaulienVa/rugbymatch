703e2dad496366f5dcbe2f9e53f2667722d41339
Paulien <>
2018-09-17 23:20:38 +0200

    [KOTLIN] - Ex2: clean-up

--- src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
@@ -1,8 +1,6 @@
 package com.paulienvanalst.rugbymatch.events
 
 import org.springframework.context.ApplicationEvent
-import com.paulienvanalst.rugbymatch.team.TeamName
-import com.paulienvanalst.rugbymatch.game.*
 
 sealed class SetPieceEvent(source: Any, val someArgs: Any) : ApplicationEvent(source)
 
