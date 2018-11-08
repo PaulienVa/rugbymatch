9f5aae764b55bf64b72a777a648b105d024425ad
Alst, P. van (Paulien) <paulien@openvalue.nl>
2018-11-08 09:31:22 +0100

    [KOTLIN] - Fixed imports

--- src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/events/GameEvents.kt
@@ -1,6 +1,7 @@
 package com.paulienvanalst.rugbymatch.events
 
-import com.paulienvanalst.rugbymatch.team.Team
+import com.paulienvanalst.rugbymatch.game.SetPiece
+import com.paulienvanalst.rugbymatch.team.TeamName
 import org.springframework.context.ApplicationEvent
 
 sealed class SetPieceEvent(source: Any, val setPiece: SetPiece, val winningTeam: TeamName) : ApplicationEvent(source)
