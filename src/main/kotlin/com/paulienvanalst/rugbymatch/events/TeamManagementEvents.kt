8a36661ba8759c08061bcff59fa994ecb784f439
Paulien van Alst <paulien@openvalue.nl>
2019-10-25 14:21:13 +0200

    [KOTLIN] - deleted exercise 4

--- src/main/kotlin/com/paulienvanalst/rugbymatch/events/TeamManagementEvents.kt
+++
@@ -1,9 +0,0 @@
-package com.paulienvanalst.rugbymatch.events
-
-import com.paulienvanalst.rugbymatch.team.Player
-import com.paulienvanalst.rugbymatch.team.TeamName
-import org.springframework.context.ApplicationEvent
-
-class SubstitutionAnnounced(source: Any, val teamName: TeamName, val startingPlayer: Player, val sub: Player) : ApplicationEvent(source)
-class SubstitutionPerformed(source: Any, val teamName: TeamName, val players: List<Player>) : ApplicationEvent(source)
-class SquadAnnounced(source: Any, val startingPlayers: List<Player>, val teamName: TeamName) : ApplicationEvent(source)
--- src/main/kotlin/com/paulienvanalst/rugbymatch/game/Substitution.kt
+++
@@ -1,6 +0,0 @@
-package com.paulienvanalst.rugbymatch.game
-
-import com.paulienvanalst.rugbymatch.team.Player
-import com.paulienvanalst.rugbymatch.team.TeamName
-
-data class Substitution(val teamName: TeamName, val subPlayer: Player, val formerPlayer: Player)
